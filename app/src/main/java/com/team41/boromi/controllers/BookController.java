bdb03f657b4528eecd93c30376334048c52f0606
ravioli0509 <nawalage@ualberta.ca>
2020-10-22 20:30:56 +0900

    added controller/bookcontroller, skeleton for now. Will add the logics later as we integrate DB settings with others

---
+++ app/src/main/java/com/team41/boromi/controllers/BookController.java
@@ -0,0 +1,33 @@
+package com.team41.boromi.controllers;
+
+import static com.team41.boromi.constants.CommonConstants.BookStatus;
+import java.util.ArrayList;
+
+public class BookController {
+
+    public Book getOwnerBooks(){
+
+    }
+
+    public void filterBookByStatus(ArrayList<Books> books, BookStatus bookStatus){
+
+    }
+
+    public void addBook(Book book) {
+
+    }
+
+    public void deleteBook(Book book) {
+
+    }
+
+    public String getUser(String username) {
+
+    }
+
+    public String findBooks(String keyword) {
+
+    }
+
+    public void getBorrowedBooks() {}
+}
