a6deb73dea5507c0e3b1e6a267594ba879dd071f
ravioli0509 <nawalage@ualberta.ca>
2020-11-02 07:32:17 +0900

    didnt fully committed the files

--- app/src/androidTest/java/com/team41/boromi/BookControllerTest.java
+++ app/src/androidTest/java/com/team41/boromi/BookControllerTest.java
@@ -1,106 +1,186 @@
 package com.team41.boromi;
 
-
 import android.util.Log;
-import android.widget.ArrayAdapter;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.team41.boromi.controllers.BookController;
 import com.team41.boromi.dbs.BookDB;
 import com.team41.boromi.models.Book;
+import com.team41.boromi.models.User;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import com.team41.boromi.constants.CommonConstants.BookWorkflowStage;
 import com.team41.boromi.constants.CommonConstants.BookStatus;
 import static com.team41.boromi.constants.CommonConstants.DB_TIMEOUT;
 import com.team41.boromi.callbacks.BookCallback;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
+@RunWith(AndroidJUnit4.class)
 public class BookControllerTest {
+    private static BookDB bookDB;
+    private static BookController bookController;
     private final static String TAG = "BookControllerTest";
-    protected BookStatus status;
-    protected BookWorkflowStage workflow;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    BookDB bookDB = new BookDB(db);
-    ExecutorService executorService = new ThreadPoolExecutor(1, 4, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>() );
-    BookController bookController = new BookController(bookDB, executorService, db);
+
+    BookStatus status;
     ArrayList<Book> testBooks = new ArrayList<>();
 
+    private static String brock_uuid;
+    private static String ben_uuid;
+    private static String ming_uuid;
+    private static String andy_uuid;
+
+    /**
+     * Setting up users for uuid, books for test cases and adding them into db
+     */
     @Before
-    public void setup(){
-        testBooks.add(new Book("testuser1", "The Hobbit", "JK Rowling", "123456789012"));
+    public void setup() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        bookDB = new BookDB(db);
+        ExecutorService executorService = new ThreadPoolExecutor(1, 6, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>() );
+        bookController = new BookController(bookDB, executorService, db);
+
+        User lil_brock = new User("1234a", "Lil Brock", "lilbrock@gmail.com");
+        User lil_ming = new User("5678b", "Lil Ming", "lilming@gmail.com");
+        User lil_ben = new User("8901c", "Lil Ben", "lilben@gmail.com");
+        User lil_andy = new User("2345d", "Lil Andy", "lilandy@gmail.com");
+
+        brock_uuid = lil_brock.getUUID();
+        ben_uuid = lil_ben.getUUID();
+        ming_uuid = lil_ming.getUUID();
+        andy_uuid = lil_andy.getUUID();
+
+        Book booktest1 = new Book(brock_uuid, "Narnia 1", "JK Rowling", "123456789020");
+        booktest1.setStatus(status.AVAILABLE);
+
+        Book booktest2 = new Book(brock_uuid, "Narnia 2", "JK Rowling", "123456789021");
+        booktest2.setStatus(status.REQUESTED);
+
+        Book booktest3 = new Book(brock_uuid, "Narnia 3", "JK Rowling", "123456789022");
+        booktest3.setStatus(status.ACCEPTED);
+
+        Book booktest4 = new Book(brock_uuid, "Narnia 4", "JK Rowling", "123456789023");
+        booktest4.setStatus(status.BORROWED);
+
+        Book booktest5 = new Book(ming_uuid, "Harry Potter and the Chamber of Secrets", "JK Rowling", "123456789020");
+
+        Book booktest6 = new Book(ming_uuid, "Harry Potter and the Philosopher's Stone", "JK Rowling", "123456789021");
+
+        Book booktest7 = new Book(ming_uuid, "Toy Story 3", "Ming", "123456789022");
+
+        Book booktest8 = new Book(ben_uuid, "Damn Ben Dipped", "Ming", "123456789023");
+
+        testBooks.add(booktest1);
+        testBooks.add(booktest2);
+        testBooks.add(booktest3);
+        testBooks.add(booktest4);
+        testBooks.add(booktest5);
+        testBooks.add(booktest6);
+        testBooks.add(booktest7);
+        testBooks.add(booktest8);
+
+        for (Book eachBook : testBooks) {
+            bookDB.pushBook(eachBook);
+        }
     }
 
+    /**
+     * Clears DB, and array list of books
+     */
     @After
-    public void tearDown(){
+    public void tearDown() {
+        for (Book eachBook : testBooks) {
+            bookDB.deleteBook(eachBook.getBookId());
+        }
         testBooks.clear();
     }
 
+    /**
+     * Test case for adding new book and pushing to DB
+     * @throws InterruptedException
+     */
     @Test
     public void testAdd() throws InterruptedException {
-        String author = "Check workflow test";
-        String owner = "RaviTest";
+        String author = "Test Add";
+        String owner = "lilravi121";
         String ISBN = "127837122";
         String title = "FooBar";
+        Log.d(TAG, "==== TEST ADD ====");
         bookController.addBook(owner, author, ISBN, title, new BookCallback() {
             @Override
             public void onSuccess(ArrayList<Book> books) {
                 ArrayList<Book> test = bookDB.getUsersOwnedBooks(owner);
                 assertEquals(test.size(), 1);
+                bookDB.deleteBook(books.get(0).getBookId());
             }
 
             @Override
             public void onFailure(Exception e) {
-                Log.d(TAG, "Success");
                 Log.d(TAG, "FAILED");
                 fail();
             }
         });
         Thread.sleep(DB_TIMEOUT);
+        Log.d(TAG, "==== END TEST ADD ====");
     }
 
+    /**
+     * Testing for edit
+     * @throws InterruptedException
+     */
     @Test
     public void testEdit() throws InterruptedException{
-        String bookID = "3f095b91-b25a-44a4-aee8-59160f59c17b";
-        String title = "Test Edit";
-        String author = "JK Rowling";
-        String ISBN = "12312313";
+        Log.d(TAG, "==== TEST EDIT ====");
+        String title = "Test Ming";
+        String author = "Ming";
+        String ISBN = "123456789022";
+        Book booktest = new Book(ming_uuid, title, author, ISBN);
+        bookDB.pushBook(booktest);
+        String bookID = booktest.getBookId();
+        title = "Test Edit";
         bookController.editBook(bookID, author, ISBN, title, new BookCallback() {
             @Override
             public void onSuccess(ArrayList<Book> books) {
                 Book booktest = bookDB.getBook(bookID);
                 Log.d(TAG, "Success");
                 assertEquals(booktest.getTitle(), "Test Edit");
+                bookDB.deleteBook(books.get(0).getBookId());
             }
 
             @Override
             public void onFailure(Exception e) {
-                Log.d(TAG, "Success");
                 Log.d(TAG, "FAILED");
                 fail();
             }
         });
         Thread.sleep(DB_TIMEOUT);
-
+        Log.d(TAG, "==== END TEST EDIT ====");
     }
 
+    /**
+     * Testing for editing book and pushing to DB
+     * @throws InterruptedException
+     */
     @Test
     public void testDelete() throws InterruptedException {
+        Log.d(TAG, "==== TEST DELETE ====");
         Book booktest = new Book("MingDelete", "Toy Story 3", "Ming", "123456789022");
+        bookDB.pushBook(booktest);
         String bookID = booktest.getBookId();
         bookController.deleteBook(bookID, new BookCallback() {
             @Override
@@ -117,140 +197,23 @@ public class BookControllerTest {
             }
         });
         Thread.sleep(DB_TIMEOUT);
-
-
         Book test = bookDB.getBookById(bookID);
         assertNull(test);
-        Thread.sleep(DB_TIMEOUT);
+        Log.d(TAG, "==== END TEST DELETE ====");
     }
 
-    @Test
-    public void testOwnerBooksByFilter() throws InterruptedException {
-        String owner = "testuser1";
-
-        Book booktest1 = new Book(owner, "Toy Story 1", "JK Rowling", "123456789020");
-        booktest1.setStatus(status.AVAILABLE);
-        booktest1.setWorkflow(workflow.AVAILABLE);
-
-        Book booktest2 = new Book(owner, "Toy Story 2", "JK Rowling", "123456789021");
-        booktest2.setStatus(status.REQUESTED);
-        booktest2.setWorkflow(workflow.REQUESTED);
-
-        Book booktest3 = new Book(owner, "Toy Story 3", "JK Rowling", "123456789022");
-        booktest3.setStatus(status.ACCEPTED);
-        booktest3.setWorkflow(workflow.ACCEPTED);
-
-        Book booktest4 = new Book(owner, "Toy Story 4", "JK Rowling", "123456789023");
-        booktest4.setStatus(status.BORROWED);
-        booktest4.setWorkflow(workflow.PENDINGRETURN);
-
-        Book booktest5 = new Book(owner, "Toy Story 5", "JK Rowling", "123456789024");
-        booktest5.setStatus(status.AVAILABLE);
-        booktest5.setWorkflow(workflow.AVAILABLE);
-
-        Book booktest6 = new Book(owner, "Toy Story 6", "JK Rowling", "123456789025");
-        booktest6.setStatus(status.ACCEPTED);
-        booktest6.setWorkflow(workflow.ACCEPTED);
-
-        Book booktest7 = new Book(owner, "Toy Story 7", "JK Rowling", "123456789026");
-        booktest7.setStatus(status.REQUESTED);
-        booktest7.setWorkflow(workflow.REQUESTED);
-
-        Book booktest8 = new Book(owner, "Toy Story 8", "JK Rowling", "123456789027");
-        booktest8.setStatus(status.BORROWED);
-        booktest8.setWorkflow(workflow.PENDINGRETURN);
-
-        bookDB.pushBook(booktest1);
-        bookDB.pushBook(booktest2);
-        bookDB.pushBook(booktest3);
-        bookDB.pushBook(booktest4);
-        bookDB.pushBook(booktest5);
-        bookDB.pushBook(booktest6);
-        bookDB.pushBook(booktest7);
-        bookDB.pushBook(booktest8);
-
-        bookController.getOwnerAvailableBooks(owner, new BookCallback() {
-            @Override
-            public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
-                assertEquals(books.size(), 2);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "FAILED");
-                fail();
-            }
-        });
-        Thread.sleep(DB_TIMEOUT);
-
-        bookController.getOwnerRequestedBooks(owner, new BookCallback() {
-            @Override
-            public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
-                assertEquals(books.size(), 2);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "FAILED");
-                fail();
-            }
-        });
-        Thread.sleep(DB_TIMEOUT);
-
-        bookController.getOwnerAcceptedBooks(owner, new BookCallback() {
-            @Override
-            public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
-                assertEquals(books.size(), 2);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "FAILED");
-                fail();
-            }
-        });
-        Thread.sleep(DB_TIMEOUT);
-
-        bookController.getOwnerBorrowedBooks(owner, new BookCallback() {
-            @Override
-            public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
-                assertEquals(books.size(), 2);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "FAILED");
-                fail();
-            }
-        });
-        Thread.sleep(DB_TIMEOUT);
-
-        bookController.getOwnerAvailableBooks("Ming Test", new BookCallback() {
-            @Override
-            public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "FAILED");
-                fail();
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "Success");
-                assertTrue(true);
-            }
-        });
-        Thread.sleep(DB_TIMEOUT);
-    }
+    /**
+     * Testing where the filter functions work. (Filter by book statuses)
+     * @throws InterruptedException
+     */
 
     @Test
-    public void testGetOwnedBooks() throws InterruptedException {
-        bookController.getOwnedBooks("testuser1", new BookCallback() {
+    public void testOwnerBooksByFilterStatus() throws InterruptedException {
+        Log.d(TAG, "==== TEST FILTER OWNER BY STATUS ====");
+        bookController.getOwnerAvailableBooks(brock_uuid, new BookCallback() {
             @Override
             public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
+                Log.d(TAG, "Success: Lil Brock & AVAILABLE");
                 assertEquals(books.size(), 1);
             }
 
@@ -261,31 +224,113 @@ public class BookControllerTest {
             }
         });
         Thread.sleep(DB_TIMEOUT);
+
+        bookController.getOwnerRequestedBooks(brock_uuid, new BookCallback() {
+            @Override
+            public void onSuccess(ArrayList<Book> books) {
+                Log.d(TAG, "Success: Lil Brock & REQUESTED");
+                assertEquals(books.size(), 1);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.d(TAG, "FAILED");
+                fail();
+            }
+        });
+        Thread.sleep(DB_TIMEOUT);
+
+        bookController.getOwnerAcceptedBooks(brock_uuid, new BookCallback() {
+            @Override
+            public void onSuccess(ArrayList<Book> books) {
+                Log.d(TAG, "Success: Lil Brock & ACCEPTED");
+                assertEquals(books.size(), 1);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.d(TAG, "FAILED");
+                fail();
+            }
+        });
+        Thread.sleep(DB_TIMEOUT);
+
+        bookController.getOwnerBorrowedBooks(brock_uuid, new BookCallback() {
+            @Override
+            public void onSuccess(ArrayList<Book> books) {
+                Log.d(TAG, "Success: Lil Brock & BORROWED");
+                assertEquals(books.size(), 1);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.d(TAG, "FAILED");
+                fail();
+            }
+        });
+        Thread.sleep(DB_TIMEOUT);
+
+        bookController.getOwnerAvailableBooks(andy_uuid, new BookCallback() {
+            @Override
+            public void onSuccess(ArrayList<Book> books) {
+                Log.d(TAG, "Success: NO RESULTS FOR LIL ANDREW");
+                assertEquals(books.size(), 0);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.d(TAG, "Failed");
+                fail();
+            }
+        });
+        Thread.sleep(DB_TIMEOUT);
+
+        Log.d(TAG, "==== END TEST FILTER OWNER BY STATUS ====");
     }
 
+    /**
+     * Test to see if it returns owner specified books
+     * @throws InterruptedException
+     */
+    @Test
+    public void testGetOwnedBooks() throws InterruptedException {
+        Log.d(TAG, "==== TEST GET BOOKS BY OWNER ====");
+        bookController.getOwnedBooks(ben_uuid, new BookCallback() {
+            @Override
+            public void onSuccess(ArrayList<Book> books) {
+                String title_1 = books.get(0).getTitle();
+                Log.d(TAG, "Success, here are the results: "+ title_1);
+                assertEquals(books.size(), 1);
+                assertEquals(title_1, "Damn Ben Dipped");
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.d(TAG, "FAILED!");
+                fail();
+            }
+        });
+        Thread.sleep(DB_TIMEOUT);
+        Log.d(TAG, "==== END GET BOOKS BY OWNER ====");
+    }
+
+    /**
+     * Test if search book function works.
+     * @throws InterruptedException
+     */
     @Test
     public void testFindBooks() throws InterruptedException {
-        Book booktest1 = new Book("testuser1", "Harry Potter and the Chamber of Secrets", "JK Rowling", "123456789020");
-        booktest1.setStatus(status.AVAILABLE);
-        booktest1.setWorkflow(workflow.AVAILABLE);
-
-        Book booktest2 = new Book("testuser1", "Harry Potter and the Philosopher's Stone", "JK Rowling", "123456789021");
-        booktest2.setStatus(status.REQUESTED);
-        booktest2.setWorkflow(workflow.REQUESTED);
-
-        Book booktest3 = new Book("testuser1", "Toy Story 3", "Ming", "123456789022");
-        booktest3.setStatus(status.ACCEPTED);
-        booktest3.setWorkflow(workflow.ACCEPTED);
-
-        bookDB.pushBook(booktest1);
-        bookDB.pushBook(booktest2);
-        bookDB.pushBook(booktest3);
-
+        Log.d(TAG, "==== TEST FIND BOOKS USING KEYWORDS ====");
         bookController.findBooks("Harry", new BookCallback() {
             @Override
             public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
+                Collections.sort(books, (book_1, book_2) -> book_1.getTitle().compareTo(book_2.getTitle()));
+                String title_2 = books.get(1).getTitle();
+                String title_1 = books.get(0).getTitle();
+                Log.d(TAG, "Success! Here are the results: " + title_1 + ", " + title_2);
                 assertEquals(books.size(), 2);
+                assertEquals(title_2, "Harry Potter and the Philosopher's Stone");
+                assertEquals(title_1, "Harry Potter and the Chamber of Secrets");
             }
 
             @Override
@@ -297,27 +342,13 @@ public class BookControllerTest {
 
         Thread.sleep(DB_TIMEOUT);
 
-        bookController.findBooks("", new BookCallback() {
+        bookController.findBooks("Story", new BookCallback() {
             @Override
             public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
-                assertNull(books);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(TAG, "FAILED");
-                fail();
-            }
-        });
-
-        Thread.sleep(DB_TIMEOUT);
-
-        bookController.findBooks("Chamber", new BookCallback() {
-            @Override
-            public void onSuccess(ArrayList<Book> books) {
-                Log.d(TAG, "Success");
+                String title_1 = books.get(0).getTitle();
+                Log.d(TAG, "Success! Here are the results :"+title_1);
                 assertEquals(books.size(), 1);
+                assertEquals(title_1, "Toy Story 3");
             }
 
             @Override
@@ -327,7 +358,24 @@ public class BookControllerTest {
             }
         });
 
-    }
+        Thread.sleep(DB_TIMEOUT);
 
+        bookController.findBooks("Ming", new BookCallback() {
+            @Override
+            public void onSuccess(ArrayList<Book> books) {
+                Log.d(TAG, "Failed!");
+                fail();
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.d(TAG, "No results");
+                assertTrue(true);
+            }
+        });
+
+        Thread.sleep(DB_TIMEOUT);
+        Log.d(TAG, "==== END TEST FIND BOOKS USING KEYWORDS ====");
+    }
 
 }
